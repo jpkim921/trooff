1da498cd22f5fad188ccbcb9f63c4855b5213c8a
Jayon Kim <jpkim921@gmail.com>
2020-05-09 12:19:06 -0400

    include accounts.urls in trooff.urls

--- troof/urls.py
+++ troof/urls.py
@@ -16,16 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from pages.views import home_page_view, contact_page_view
-from donations.views import donations_view, donation_form_page_view, donation_donor_form
+# from pages.views import home_page_view, contact_page_view
+# from donations.views import donations_view, donation_form_page_view, donation_donor_form
+
+# urlpatterns = [
+#     path('', include('products.urls')),
+#     path('', home_page_view),
+#     path('donations/', donations_view),
+#     # path('donated/', donation_form_page_view),
+#     path('donate/', donation_donor_form),
+#     path('contact/', contact_page_view),
+#     path('admin/', admin.site.urls),
+# ]
 
 
 urlpatterns = [
-    path('', include('products.urls')),
-    path('', home_page_view),
-    path('donations/', donations_view),
-    # path('donated/', donation_form_page_view),
-    path('donate/', donation_donor_form),
-    path('contact/', contact_page_view),
+    path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
+
 ]
