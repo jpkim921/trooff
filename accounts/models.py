83cb712a35dae8d9105a79641d6d9221f6cf081c
Jayon Kim <jpkim921@gmail.com>
2020-05-13 10:13:37 -0400

    add onetoone relationship between User and Customer

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 from products.models import Product
+from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
