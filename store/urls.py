5a2cf25f9a01c41833d62fe613ef43f36e03c359
Jayon Kim <jpkim921@gmail.com>
2020-05-15 15:29:05 -0400

    add product page

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('store/', views.ProductsView, name='store'),
+    path('product/<int:pk>/', views.ProductView, name='product'),
 
 ]
--- store/views.py
+++ store/views.py
@@ -4,6 +4,10 @@ from .models import Product
 
 def ProductsView(request):
     products = Product.objects.all()
-    print(products)
     context = {'products': products}
     return render(request, 'store/store.html', context)
+
+def ProductView(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {'product': product}
+    return render(request, 'store/product.html', context)
