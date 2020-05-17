14c9f950ef7ccf991d1e4cffaae739c93abb5df6
Jayon Kim <jpkim921@gmail.com>
2020-05-17 17:34:22 -0400

    add gt and lt price filters

--- store/filters.py
+++ store/filters.py
@@ -1,13 +1,17 @@
 import django_filters
-from django_filters import CharFilter
+from django_filters import CharFilter, NumberFilter
 
 from .models import *
 
 
 class ProductFilter(django_filters.FilterSet):
         name = CharFilter(field_name="name", lookup_expr='icontains')
-
+        # price = django_filters.NumberFilter()
+        price__gt = NumberFilter(field_name='price', lookup_expr='gt')
+        price__lt = NumberFilter(field_name='price', lookup_expr='lt')
+        
         class Meta:
             model = Product
-            fields = '__all__'
-            exclude = ['date', 'description', 'tags']
+            # fields = '__all__'
+            fields = ['name', 'category']
+            # exclude = ['date', 'description', 'tags']
