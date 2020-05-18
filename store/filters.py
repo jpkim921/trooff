e3f07c514b071178eb911c2888d9d8eaa5cf401c
Jayon Kim <jpkim921@gmail.com>
2020-05-18 19:36:32 -0400

    change ChoiceFilter to MultipleChoiceFilter

--- store/filters.py
+++ store/filters.py
@@ -1,20 +1,14 @@
 import django_filters
-from django_filters import CharFilter, NumberFilter, ChoiceFilter
+from django_filters import CharFilter, NumberFilter, MultipleChoiceFilter
 from django import forms
 
 from .models import *
 
-
 class ProductFilter(django_filters.FilterSet):
-    CATEGORY = (
-        ('Cup', 'Cup'),
-        ('Plate', 'Plate'),
-        ('Bowl', 'Bowl'),
-        ('Vase', 'Vase')
-    )
+
     name = CharFilter(field_name="name",
                       lookup_expr='icontains', label="Product Name", widget=forms.TextInput(attrs={'class': 'form-control form-control-sm'}))
-    category = ChoiceFilter(choices=CATEGORY, field_name='category', label="Category",
+    category = MultipleChoiceFilter(choices=Product.CATEGORY, field_name='category', label="Category",
                             widget=forms.SelectMultiple(attrs={'class': 'form-control form-control-sm'}))
     price__gt = NumberFilter(field_name='price',
                              lookup_expr='gt', label="Minimum Price", widget=forms.NumberInput(attrs={'class': 'form-control form-control-sm'}))
