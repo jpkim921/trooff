ca4de298ce267b8e8829b60f29e29a3d0e132490
Jayon Kim <jpkim921@gmail.com>
2020-05-18 11:47:25 -0400

    add class to filter forms elements

--- store/filters.py
+++ store/filters.py
@@ -1,17 +1,18 @@
 import django_filters
 from django_filters import CharFilter, NumberFilter
+from django import forms
 
 from .models import *
 
 
 class ProductFilter(django_filters.FilterSet):
     name = CharFilter(field_name="name",
-                      lookup_expr='icontains', label="Product Name")
-    # price = django_filters.NumberFilter()
+                      lookup_expr='icontains', label="Product Name", widget=forms.TextInput(attrs={'class': 'form-control'}))
+                      
     price__gt = NumberFilter(field_name='price',
-                             lookup_expr='gt', label="Minimum Price")
+                             lookup_expr='gt', label="Minimum Price", widget=forms.NumberInput(attrs={'class': 'form-control'}))
     price__lt = NumberFilter(field_name='price',
-                             lookup_expr='lt', label="Maximum Price")
+                             lookup_expr='lt', label="Maximum Price", widget=forms.NumberInput(attrs={'class': 'form-control'})
 
     class Meta:
         model = Product
