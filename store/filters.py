f11132aca3af40378c2de8379d350f7d3a3af35a
Jayon Kim <jpkim921@gmail.com>
2020-05-18 10:50:11 -0400

    change labels for filter form elements

--- store/filters.py
+++ store/filters.py
@@ -5,13 +5,16 @@ from .models import *
 
 
 class ProductFilter(django_filters.FilterSet):
-        name = CharFilter(field_name="name", lookup_expr='icontains')
-        # price = django_filters.NumberFilter()
-        price__gt = NumberFilter(field_name='price', lookup_expr='gt')
-        price__lt = NumberFilter(field_name='price', lookup_expr='lt')
-        
-        class Meta:
-            model = Product
-            # fields = '__all__'
-            fields = ['name', 'category']
-            # exclude = ['date', 'description', 'tags']
+    name = CharFilter(field_name="name",
+                      lookup_expr='icontains', label="Product Name")
+    # price = django_filters.NumberFilter()
+    price__gt = NumberFilter(field_name='price',
+                             lookup_expr='gt', label="Minimum Price")
+    price__lt = NumberFilter(field_name='price',
+                             lookup_expr='lt', label="Maximum Price")
+
+    class Meta:
+        model = Product
+        # fields = '__all__'
+        fields = ['name', 'category']
+        # exclude = ['date', 'description', 'tags']
